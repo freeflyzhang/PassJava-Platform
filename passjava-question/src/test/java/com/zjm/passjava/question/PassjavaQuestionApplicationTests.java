35f99138ffa0a8dd5101bbd638bf52011f12a618
freeflyzhang <17858765682@163.com>
2021-08-25 14:59:59 +0800

    框架

---
+++ passjava-question/src/test/java/com/zjm/passjava/question/PassjavaQuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjm.passjava.question;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaQuestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
